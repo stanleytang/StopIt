8cad4b18bc39d3634de8bf55172f6877b5611235
Stanley Tang <account@stanleytang.com>
2012-10-25 22:23:52 -0700

    refactored code

---
+++ stopit/media/js/map.js
@@ -0,0 +1,153 @@
+function MapModule(id) {
+  this.mapCanvas = document.getElementById(id);
+  this.map = new google.maps.Map(
+    this.mapCanvas,
+    {
+      zoom: 14,
+      // initially center on Stanford
+      center: new google.maps.LatLng(37.4225, -122.1653),
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+  );
+  
+  this.buildMap();
+  this.trackUserLocation();
+}
+
+MapModule.prototype.fetchAndshowStopsInArea = function() {
+  if (!this.alreadyDisplayed) { // temp hack
+    /****** TEMPORARY STOPS DUMMY DATA ********/
+    var stop1 = {
+      title: "Palo Alto Station",
+      latitude: 37.4419,
+      longitude: -122.1649,
+    };
+  
+    var stop2 = {
+      title: "The Oval",
+      latitude: 37.4290,
+      longitude: -122.1706,
+    };
+  
+    var stop3 = {
+      title: "Vaden Health Center",
+      latitude: 37.4220,
+      longitude: -122.1624,
+    };
+  
+    var stopArray = [stop1, stop2, stop3];
+  
+    this.displayStopsOnMap(stopArray);
+    this.alreadyDisplayed = true;
+  }
+}
+
+MapModule.prototype.buildMap = function() {
+  // Set the map canvas's height/width (Google Maps needs inline height/width)
+	var height = $(window).height() - $("div[data-role='header']").height() + 60;
+	this.mapCanvas.style.width = '100%';
+	this.mapCanvas.style.height = height + 'px';
+}
+
+MapModule.prototype.trackUserLocation = function() {
+  var useragent = navigator.userAgent;
+  var obj = this;
+	
+	var displayUserLocationOnMap = function(position) {
+    // Create a new LatLng object for every position update
+	  var myLatLng = new google.maps.LatLng(
+  	  position.coords.latitude, 
+  	  position.coords.longitude
+  	);
+
+  	// Build entire marker if first time 
+  	if (!obj.locationMarker) {
+  		// Define our custom marker image
+  		var image = new google.maps.MarkerImage(
+  			'/media/images/bluedot.png',
+  			null, // size
+  			null, // origin
+  			new google.maps.Point( 8, 8 ), // anchor (move to center of marker)
+  			new google.maps.Size( 17, 17 ) // scaled size (for Retina display icon)
+  		);
+
+  		// Then create the new marker
+  		obj.locationMarker = new google.maps.Marker({
+  			flat: true,
+  			icon: image,
+  			map: obj.map,
+  			optimized: false,
+  			position: myLatLng,
+  			title: 'My location',
+  			visible: true
+  		});
+		
+  		// Center map view
+  		obj.map.setCenter(myLatLng);
+	
+  	// Just change marker position on subsequent passes
+  	} else {
+  		obj.locationMarker.setPosition(myLatLng);
+  	}
+  }
+  
+  var handleLocationError = function(error) {
+    var errorMessage = [ 
+  		'We are not quite sure what happened.',
+  		'Sorry. Permission to find your location has been denied.',
+  		'Sorry. Your position could not be determined.',
+  		'Sorry. Timed out.'
+  	];
+  	alert(errorMessage[error.code]);
+  }
+  
+  // Allow iPhone or Android to track movement
+	if (useragent.indexOf('iPhone') !== -1 || 
+	    useragent.indexOf('Android') !== -1 ) {
+		navigator.geolocation.watchPosition( 
+		  displayUserLocationOnMap, 
+			handleLocationError, 
+			{ 
+				enableHighAccuracy: true, 
+				maximumAge: 30000, 
+				timeout: 27000 
+			}
+		);			
+
+	// Or let other geolocation capable browsers to get their static position
+	} else if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(
+		  displayUserLocationOnMap, 
+		  handleLocationError
+		);
+	}
+}
+
+MapModule.prototype.displayStopsOnMap = function(stopsInfoArray) {
+  // Consider using 
+  // http://jquery-ui-map.googlecode.com/svn/trunk/demos/jquery-google-maps-mobile.html
+  
+  for (index in stopsInfoArray) {
+    var stop = stopsInfoArray[index];
+    var stopLatLng = new google.maps.LatLng(stop.latitude, stop.longitude);
+    
+    // Create marker
+    var stopMarker = new google.maps.Marker({
+      position: stopLatLng,
+      map: this.map,
+      title: stop.title
+    });
+
+    // Create popup window
+    var stopInfoWindow = new google.maps.InfoWindow();
+    google.maps.event.addListener(stopMarker, 'click', function() {
+      var content = "<a href='/stop/'>" + this.title + "</a>";
+      stopInfoWindow.setContent(content);
+      stopInfoWindow.open(this.map, this);
+    });
+  }
+}
+
+MapModule.prototype.clearStopsOnMap = function() {
+  // https://developers.google.com/maps/documentation/javascript/overlays#RemovingOverlays
+}
