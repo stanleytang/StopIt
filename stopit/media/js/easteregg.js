7b93a655751e79c29d8c25ccea19678867c7e81f
wendinglu <wending@stanford.edu>
2012-11-27 23:55:22 -0800

    Changed easter egg to use html5

--- stopit/media/js/easteregg.js
+++ stopit/media/js/easteregg.js
@@ -1,9 +1,7 @@
 function easterEgg() {
 	var soundfile = "/media/audio/stopit" + Math.floor(1 + Math.random() * 3)
-		+ ".wav";
-	var innerHTML = 		
-		"<embed src=" + "'" + soundfile + "'" + 
-		" hidden='true' autostart='true' loop='false' />";
-	document.getElementById("dummy").innerHTML = innerHTML;
-
+		+ ".mp3";
+	document.getElementById("dummy").innerHTML = "<audio id='audioEasterEgg' src=" + "'" + soundfile + "'" + 
+		" />";
+		document.getElementById('audioEasterEgg').play();
 }
